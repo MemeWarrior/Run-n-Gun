89bbc70801d2ba3c0528b076601cec0091ed298f
MemeWarrior <101115572+MemeWarrior@users.noreply.github.com>
2022-06-27 14:29:33 -0400

    Added Sky Scene with a move script

--- Run n Gun Dungeons/Assets/Scripts/PlayerManager.cs
+++ Run n Gun Dungeons/Assets/Scripts/PlayerManager.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    Rigidbody2D body;
+
+    float horizontal;
+    float vertical;
+
+    public float speed = 20.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical"); 
+    }
+
+    private void FixedUpdate()
+    {  
+        body.velocity = new Vector2(horizontal * speed, vertical * speed);
     }
 }
