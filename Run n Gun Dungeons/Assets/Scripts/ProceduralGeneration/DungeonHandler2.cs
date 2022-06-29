1ed6b044d1d7ec17295f8a878e2fd12d2e6784fe
MemeWarrior <101115572+MemeWarrior@users.noreply.github.com>
2022-06-28 20:59:47 -0400

    No changes

--- Run n Gun Dungeons/Assets/Scripts/ProceduralGeneration/DungeonHandler2.cs
+++ Run n Gun Dungeons/Assets/Scripts/ProceduralGeneration/DungeonHandler2.cs
@@ -28,11 +28,48 @@ public class DungeonHandler2 : SimpleRandomWalkGenerator
     CreateCorridors(floorPos, potRoomPos);
 
     HashSet<Vector2Int> roomPos = CreateRooms(potRoomPos);
+    List<Vector2Int> deadEnds = FindDeadEnds(floorPos);
+    CreateRoomsAtDeadEnd(deadEnds, roomPos);
 
     floorPos.UnionWith(roomPos);
 
     TileMapVisualizer.paintFloorTiles(floorPos);
     WallHandler.createWalls(floorPos, TileMapVisualizer);
+  }
+
+  private List<Vector2Int> FindDeadEnds(HashSet<Vector2Int> floorPos)
+  {
+    List<Vector2Int> deadEnds = new List<Vector2Int>();
+    foreach (var position in floorPos)
+    {
+      int neighborsCount = 0;
+
+      foreach (var direction in direction2D.cardinalDirectionsList)
+      {
+        if(floorPos.Contains(position+direction))
+        {
+          neighborsCount++;
+        }
+      }
+
+      if(neighborsCount == 1)
+      {
+        deadEnds.Add(position);
+      }
+    }
+    return deadEnds;
+  }
+
+  private void CreateRoomsAtDeadEnd(List<Vector2Int> deadEnds, HashSet<Vector2Int> roomPos)
+  {
+    foreach (var position in deadEnds)
+    {
+      if(roomPos.Contains(position) == false)
+      {
+        var room = RunRandomWalk(randWalkParams, position);
+        roomPos.UnionWith(room);
+      }
+    }
   }
 
   private HashSet<Vector2Int> CreateRooms(HashSet<Vector2Int> potRoomPos)
