a184d6b23d2a9af6e3ba98e0dbeaa37b3a02042a
AndyLeq129 <97884873+AndyLeq129@users.noreply.github.com>
2022-07-01 10:25:00 -0400

    Resources Update

---
+++ Run n Gun Dungeons/Assets/Scripts/Weapons/Bullet.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int damage;
+    public float speed;
+    public float arrowDistance;
+
+    private Rigidbody2D rg2d;
+    private Vector3 startPos;
+
+    private CameraShake shake;
+
+    void Start()
+    {
+        rg2d = GetComponent<Rigidbody2D>();
+        rg2d.velocity = transform.right * speed;
+        startPos = transform.position;
+
+        shake = GameObject.FindGameObjectWithTag("ScreenShake").GetComponent<CameraShake>();
+    }
+
+    void Update()
+    {
+        float distance = (transform.position - startPos).sqrMagnitude;
+        if (distance > arrowDistance)
+        {
+            //Instantiate(destroyEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    } 
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            shake.CamShake();
+            //Instantiate(destroyEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+            //other.GetComponent<Enemy>().TakeDamage(damage);
+        }
+    }
+}
